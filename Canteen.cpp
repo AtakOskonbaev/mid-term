09baaef72decd5063308945c9923857d108ebeb9
Atak Oskonbaev <atakoskonbaev21@gmail.com>
2023-11-18 22:30:55 +0600

    added solutions

--- Canteen.cpp
+++ Canteen.cpp
@@ -1,4 +1,12 @@
+#include <iostream>
+using namespace std;
 int main()
 {
-    return 0;
+    int A, B, N;
+    cin >> A >> B >> N;
+    A = A * N;
+    B = B * N;
+    A += B / 100;
+    B = B % 100;
+    cout << A << " som and " << B << " tyiyn";
 }
